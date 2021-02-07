fab564c8b923def1e9012df3cfcae4201758cbeb
Ameya <ranadeameya@outlook.com>
2021-02-07 22:22:11 +0100

    SPACE QUOTA SETTINGS ADDED

--- src/ServerListItem.cpp
+++ src/ServerListItem.cpp
@@ -9,23 +9,42 @@
 
 #include <bb/cascades/Container>
 #include <bb/cascades/DockLayout>
+#include <bb/cascades/StackLayout>
+#include <bb/cascades/StackLayoutProperties>
 
 using namespace bb::cascades;
 
 ServerListItem::ServerListItem()
 {
     Container *serverItemContainer = new Container();
-    serverItemContainer->setBackground(Color::Green);
     serverItemContainer->setHorizontalAlignment(HorizontalAlignment::Fill);
     serverItemContainer->setVerticalAlignment(VerticalAlignment::Fill);
+    serverItemContainer->setLayout(StackLayout::create().
+                                    orientation(LayoutOrientation::LeftToRight));
 
-    serverItemContainer->setLayout(DockLayout::create());
+    Container *logoContainer = new Container();
+    logoContainer->setVerticalAlignment(VerticalAlignment::Fill);
+    logoContainer->setMinWidth(200);
+    logoContainer->setBackground(Color::Green);
+    logoContainer->setLayout(DockLayout::create());
+    logoContainer->setLayoutProperties(StackLayoutProperties::create()
+                                        .spaceQuota(0.49));
+
+    Container *labelContainer = new Container();
+    labelContainer->setVerticalAlignment(VerticalAlignment::Fill);
+    labelContainer->setMinWidth(200);
+    labelContainer->setBackground(Color::Red);
+    labelContainer->setLayout(DockLayout::create());
+    labelContainer->setLayoutProperties(StackLayoutProperties::create()
+                                            .spaceQuota(0.5));
 
     this->label = new Label();
 
     this->label->setVerticalAlignment(VerticalAlignment::Center);
     this->label->setHorizontalAlignment(HorizontalAlignment::Center);
-    serverItemContainer->add(this->label);
+    labelContainer->add(this->label);
+    serverItemContainer->add(logoContainer);
+    serverItemContainer->add(labelContainer);
     setContent(serverItemContainer);
 }
 
